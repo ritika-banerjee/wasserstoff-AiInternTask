a3b1344e00dacde2a4b595555bf9b99e4fd16770
Ritika Banerjee <109010965+ritika-banerjee@users.noreply.github.com>
2025-04-30 10:44:49 +0530

    updated js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "wasserstoff-aiinterntask-production-5a3d.up.railway.app";
 
 async function submitGuess() {
   const guess = document.getElementById("guessInput").value.trim();
